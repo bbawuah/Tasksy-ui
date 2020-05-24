413845e5bd448b0605890874a36e6ca94939d67e
bbawuah <brianbawuah@icloud.com>
2020-05-24 18:06:38 +0200

    setting up server for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,34 @@
 const path = require('path');
 
 
-module.exports = {
-  entry: ['@babel/polyfill', './src/app.js'],
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, '/public'),
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
+module.exports = (env) => {
 
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true,
-  },
-};
+  console.log('env', env)
+
+  return {
+    entry: ['@babel/polyfill', './src/app.js'],
+    output: {
+      filename: 'bundle.js',
+      path: path.join(__dirname, '/public'),
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+  
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+  };
+}
 // Finish setup
