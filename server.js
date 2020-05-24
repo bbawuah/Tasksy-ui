aae4444f5e8e543fa080d51c29279311d541f216
bbawuah <brianbawuah@icloud.com>
2020-05-24 18:36:28 +0200

    adding proxy target

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const proxy = require('http-proxy-middleware')
 
 const publicPath = path.join(__dirname, 'public')
 
 const port = process.env.PORT || 8080;
 
 app.use(express.static(publicPath));
+
+const apiProxyTarget = process.env.API_PROXY_TARGET;
+app.use(proxy({target: apiProxyTarget, changeOrigin: true}));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
